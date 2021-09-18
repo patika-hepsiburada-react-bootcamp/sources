ca53f87f3caa3cf08592525a3ee66d44b032efaf
Mehmet Seven <mehmetseven0@gmail.com>
2021-09-18 13:14:30 +0300

    var,let,const variables

---
+++ day1/var-let-const.js
@@ -0,0 +1,37 @@
+// 1.
+// let name = 'Mehmet';
+// name = 'Seven';
+
+// console.log(name);
+
+// 2.
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// console.log("i'nin son değeri: ", i);
+
+// 3.
+// const pi = 3;
+// pi = 3.14;
+// console.log(pi);
+
+// 4.
+// const users = ['Serhat', 'Ömer', 'Emine', 'Burak'];
+// users.push('Mehmet');
+// users = ['Mehmet'];
+
+// console.log(users);
+
+// 5.
+// const user = {
+//   name: 'Ahmet',
+//   surname: 'Vural',
+// };
+
+// user = {
+//   name: 'Mehmet',
+//   surname: 'Seven',
+// };
+
+// console.log(user);
