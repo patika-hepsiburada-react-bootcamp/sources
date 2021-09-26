685e0eba9ca1e9d1ea7f3c155fe9a3cffed6cea0
Mehmet Seven <mehmetseven0@gmail.com>
2021-09-26 11:40:16 +0300

    react router dom

--- day4/1-react-router/src/App.js
+++ day4/1-react-router/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Users from './pages/Users';
 import About from './pages/About';
-import UserDetail from './pages/UserDetail';
+// import UserDetail from './pages/UserDetail';
 import Error404 from './pages/Error404';
 
 function App() {
@@ -15,13 +15,19 @@ function App() {
         <nav>
           <ul className="menu">
             <li>
-              <Link to="/">Home</Link>
+              <NavLink activeClassName="active" to="/" exact>
+                Home
+              </NavLink>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <NavLink activeClassName="active" to="/about">
+                About
+              </NavLink>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <NavLink activeClassName="active" to="/users">
+                Users
+              </NavLink>
             </li>
           </ul>
         </nav>
@@ -32,8 +38,8 @@ function App() {
               <Home />
             </Route>
             <Route path="/about" component={About} />
-            <Route path="/users" exact component={Users} />
-            <Route path="/users/:id" component={UserDetail} />
+            <Route path="/users" component={Users} />
+            {/* <Route path="/users/:id" component={UserDetail} /> */}
             <Route path="*" component={Error404} />
           </Switch>
         </div>
